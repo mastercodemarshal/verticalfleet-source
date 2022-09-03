b3452dacc23393e5393edc6a8fdf90deb0e100c3
golden-lib <talentedDev0216@gmail.com>
2022-09-03 18:07:02 +0900

    updated boiler-plate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        hind: ["Hind Madurai"],
+        raleway: ["Raleway"],
+      },
+    },
   },
   plugins: [],
 };
